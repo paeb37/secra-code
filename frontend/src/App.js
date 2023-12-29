eeb80ebe52a04b4c2ff7f93229e5dcb588bd230b
paeb37 <btp2109@columbia.edu>
2023-12-29 17:33:13 -0500

    implemented login system

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,31 @@
-import React, {useState, useEffect} from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import Header from './components/Header'
+import useToken from './components/useToken'
+import './App.css'
 
 function App() {
-
-  const [data, setData] = useState([{}]);
-
-  useEffect(() => {
-    fetch("/members").then(
-      res => res.json()
-    ).then(data => {
-        setData(data);
-        console.log(data);
-    })
-  }, []);
+  const { token, removeToken, setToken } = useToken(); // useToken() is custom
 
   return (
-    <div>
-      {(typeof data.members === 'undefined') ? (
-        <p> Loading... </p>
-      ) : (
-        data.members.map((member, i) => (
-          <p key={i}>{member}</p>
-        ))
-      )}
-
-    </div>
-  )
+    // parent component
+    <BrowserRouter> 
+      <div className="App">
+        <Header token={removeToken}/>
+        {/* if no token, login */}
+        {!token && token !== "" && token !== undefined?  
+        <Login setToken={setToken} />
+        :(
+          <>
+            <Routes>
+              <Route exact path="/profile" element={<Profile token={token} setToken={setToken}/>}></Route>
+            </Routes>
+          </>
+        )}
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
