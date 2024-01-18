d29e7eb55a7612e319c12ed22089f14b9fbb986f
paeb37 <btp2109@columbia.edu>
2024-01-18 15:48:21 -0500

    tracking each file for git

--- frontend/src/components/chat/ChatPage.js
+++ frontend/src/components/chat/ChatPage.js
@@ -9,18 +9,16 @@ import NavBar from '../NavBar';
 import styles from './chat.module.css';
 import axios from 'axios'; // Import Axios
 
-import TextEntry from './TextEntry';
-import TextBubble from './TextBubble';
+import LoadingText from './LoadingText';
 
 const URL = 'auth'; // change as needed
 
 function ChatPage(props) {
 
-    return 4
-
     const logoPath = '/images/secra_logo.png';
     const [messages, setMessages] = useState(Array(0)); // empty array to start
     const navigate = useNavigate();
+    const username = props.username;
 
     // use an array of dictionaries
     // for each dict:
@@ -35,21 +33,54 @@ function ChatPage(props) {
 
     // Generate JSX code for Display each item, Add array index as the key
     // for pos, it should be "left" for assistant and "right" for user
+    /**
     const renderMessages = messages.map((item, index) => 
         <div key={index}>
             <TextBubble text={item['text']} sender={item['sender']}/>
         </div>
-    );
+    );*/
 
     return (
         <>  
-            <div className="container">
-                <h1 id="header">Your Chatbot</h1>
-                
-                {renderMessages}
 
-                <TextEntry onTextSubmit={onTextSubmit}/>
-            </div>
+        <div class={styles.container}>
+            <h1 id={styles.header}><span style={{color: 'blue'}} id="username">{username}</span>'s Assistant</h1>
+
+            <h3>Your Message</h3>
+
+            <form id="queryForm">
+                <textarea name="query" id="query" placeholder="Your Query" style={{resize: 'vertical'}} required></textarea>
+                <input type="submit" value="Submit" />
+            </form>
+        
+            <h3>Assistant Response</h3>
+
+            <p id="response">
+                <LoadingText />
+            </p>
+        </div>
+
+        <div class="container">
+            <h3>Your Calendar</h3>
+
+            <form id="calendarForm">
+                <textarea type="text" name="choice" id="choice" placeholder="Choice" required></textarea>
+                <input type="submit" value="Add to my Calendar" />
+            </form>
+        </div>
+        
+        {/*-- From tester1.secra calendar */}
+        <p align="center">
+
+        <iframe 
+            src="https://calendar.google.com/calendar/embed?src=tester1.secra%40gmail.com&ctz=America%2FNew_York" 
+            title="User's Google Calendar" 
+            style={{ border: 0 }} 
+            width="800" 
+            height="600" >
+        </iframe>
+
+        </p>
         </>
     );
 }
